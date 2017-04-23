ee829ea8a91ea3819bf2b89e3b1ef5c250e1c2b1
Robert Obreczarek <robert@obreczarek.com>
2017-04-23 14:47:55 +0200

    Added command handling to allow the user to log in and to display the credentials

--- client.js
+++ client.js
@@ -9,33 +9,48 @@ const readline = require('readline');
 const util = require('util');
 const EOL = require('os').EOL;
 
+// ## Socket.io message related ##
 const url = `http://${config.server}:${config.socketPort}`;
-
 const connection = require('socket.io-client')(url);
 
+// TODO: Define a variable keeping login/password sent on every connect.
 
+let credentials = {
+  login: 'anon-' + Math.round(Math.random() * 100),
+  password: null
+}
+
+// Fire off a message to the client when they connect
 connection.on('connect', function() {
-  writeLine('* Connected to the chat server.')
+  
+  writeLine('* Connected to the chat server.');
+  
+  if (credentials) {
+    sendLogin();
+  }
+
 })
 
+// Print a message when it is received from the server
 connection.on('message', function({ from, body }) {
   writeLine('%s: %s', from, body);
 });
 
+// Login related communication
 connection.on('login', function({ result }) {
   if (result === true) {
     writeLine('* Successfully logged in.');
   } else {
-    writeLine('* Failed to log in.')
+    writeLine('* Failed to log in.');
   }
 })
 
-connection.emit('login', {
-  login: 'user-' + Math.round(Math.random() * 100),
-  password: ''
-})
+function sendLogin() {
+  connection.emit('login', credentials);
+}
 
-// Input handling
+
+// ## Input handling
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -44,9 +59,34 @@ const rl = readline.createInterface({
 rl.setPrompt('> ');
 rl.prompt();
 
+const commandHandlers = {
+  login: function handleLogin(login, password) {
+    credentials = { login, password };
+    sendLogin();
+  },
+
+  showDetails: function() {
+    console.log(credentials);
+  }
+}
+
 rl.on('line', function(line) {
-  connection.emit('message', { body: line });
+  // Handle /commands instead of sending like normal messages
+  if (line[0] === '/') {
+    const commandParts = line.slice(1).split(' ').filter((part) => part.length > 0);
+    const commandName = commandParts[0];
+    const commandArgs = commandParts.slice(1);
+
+    if (commandHandlers[commandName]) {
+      commandHandlers[commandName].apply(undefined, commandArgs);
+    }
+  } else {
+    // This is a normal chat message
+    connection.emit('message', { body: line });  
+  }
+
   rl.prompt();
+
 })
 
 function writeLine(line, ...args) {
